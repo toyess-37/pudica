60affc20c8ec886b87adbe2177f44bfe825ffad7
toyess-37 <padhiharytaes@gmail.com>
2026-03-28 20:12:50 +0000

    updates and formatting to algo

--- codes/pudica_algo.cc
+++ codes/pudica_algo.cc
@@ -4,33 +4,40 @@
 #include <cmath>
 #include <deque>
 
-namespace PudicaAlgorithm {
-  double raw_BUR(double D_sec, double D_min_sec) {
+namespace PudicaAlgorithm
+{
+  double raw_BUR(double D_sec, double D_min_sec)
+  {
     return (D_sec - D_min_sec) / L_SEC;
   }
 
   // p = gamma_p / min(R, 1)
-  double pacing_multiplier(double R) {
+  double pacing_multiplier(double R)
+  {
     return GAMMA_P / std::min(R, 1.0);
   }
 
   // R_corrected = R + sum(T_i/L)
-  double corrected_BUR(double raw_BUR, const std::vector<double> &probe_delays_T) {
+  double corrected_BUR(double raw_BUR, const std::vector<double> &probe_delays_T)
+  {
     double R_corrected = raw_BUR;
     for (auto T_i : probe_delays_T)
       R_corrected += (T_i / L_SEC);
     return R_corrected;
   }
 
-  double smoothed_BUR(const std::deque<HistorySample> &history, double current_B) {
-    if (history.empty()) return 0.0;
+  double smoothed_BUR(const std::deque<HistorySample> &history, double current_B)
+  {
+    if (history.empty())
+      return 0.0;
 
     double weighted_sum = 0.0;
     double weight_total = 0.0;
 
     // k represents recency (1 is oldest, N is newest)
-    for (size_t i=0; i<history.size(); i++) {
-      double k = static_cast<double>(i+1);
+    for (size_t i = 0; i < history.size(); i++)
+    {
+      double k = static_cast<double>(i + 1);
       const auto &sample = history[i];
 
       // weights
@@ -50,28 +57,35 @@ namespace PudicaAlgorithm {
   }
 
   // new bitrate using MI or AI-MD
-  double calculate_next_bitrate(double current_B, double R_tilde, double frames) {
+  double calculate_next_bitrate(double current_B, double R_tilde, double frames)
+  {
     double new_B = current_B;
 
-    if (R_tilde <= ALPHA) {
+    /*
+      this does not take into consideration the "postponed until feedback" of the paper.
+      it is handled inside the listener() thread of sender.cc
+    */
+    if (R_tilde <= ALPHA)
+    {
       // MI: xi = gamma * (( (1+alpha)/2 - R_tilde ) / R_tilde)
       double xi = GAMMA_MI * (((ALPHA + 1.0) / 2.0 - R_tilde) / R_tilde);
       new_B = current_B * (1.0 + xi);
-    } else if (R_tilde <= 1.0) {
-      double safe_tau = std::min(frames/60.0, 5.0); // simultaneous AI-MD
+    }
+    else if (R_tilde <= 1.0)
+    {
+      double safe_tau = std::min(frames / 60.0, 5.0); // simultaneous AI-MD
       // adaptive AI step (I)
-      double I = (B_MAX + (std::pow(2.0, safe_tau) / std::max(std::log2(current_B), 1.0))) * (GAMMA_MD / 2.0);
+      double I = (B_MAX + (std::pow(2.0, safe_tau) / std::max(std::log(current_B), B_MIN))) * (GAMMA_MD / 2.0);
       double A = I - (GAMMA_MD * current_B); // net step A
-      
+
       // bounds on A (according to section 4.2 of the paper)
       double A_min = 0.0;
-      double A_max = A_MAX * current_B; // 0.20 is manually adjusted
-      
+      double A_max = A_MAX * current_B;
       A = std::min(std::max(A_min, A), A_max);
 
       new_B = current_B + A;
     } // steps for R > 1.0 will be addressed in the sender loop
 
-    return std::min(std::max(new_B, 1.0), static_cast<double>(B_MAX)); 
+    return std::min(std::max(new_B, 1.0), static_cast<double>(B_MAX));
   }
 }
--- codes/pudica_algo.h
+++ codes/pudica_algo.h
@@ -1,25 +1,27 @@
 #pragma once
 
-#include<vector>
-#include<deque>
+#include <vector>
+#include <deque>
 namespace PudicaAlgorithm
 {
-  constexpr double L_SEC = 16666.0 / 1'000'000.0; 
+  constexpr double L_SEC = 16666.0 / 1'000'000.0;
   constexpr double GAMMA_P = 1.25;  // pacing
   constexpr double ALPHA = 0.85;    // threshold for MI vs AI-MD
   constexpr double GAMMA_MI = 0.3;  // discounting coefficient for MI
   constexpr double GAMMA_MD = 0.05; // MD param for AI-MD
   constexpr double B_MAX = 50.0;    // maximum bitrate in Mbps
-  constexpr double A_MAX = 0.20;    // A_max = A_MAX * B (section 4.2 for bounding A)
+  constexpr double B_MIN = 1.0;     // minimum bitrate in Mbps
+  constexpr double A_MAX = 1.0;     // A_max = A_MAX * current_B; A_new = min(A_max, A_old)
 
-  struct HistorySample {
+  struct HistorySample
+  {
     double R_k; // corrected BUR of the kth frame
     double B_k; // bitrate of the kth frame in Mbps
   };
 
   double raw_BUR(double D_sec, double D_min_sec);
   double pacing_multiplier(double R);
-  double corrected_BUR(double raw_BUR, const std::vector<double>& probe_delays_T);
+  double corrected_BUR(double raw_BUR, const std::vector<double> &probe_delays_T);
   double smoothed_BUR(const std::deque<HistorySample> &history, double current_B);
   double calculate_next_bitrate(double current_B, double R_tilde, double frames);
 }
